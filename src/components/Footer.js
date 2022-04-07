a118ffb521f150e3988965f099cdd3cb45162ca5
Jared Mates <jaredmates45@gmail.com>
2022-04-07 15:06:00 -0700

    added icons to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import twitterIcon from "../images/Twitter-Icon.png";
+import facebookIcon from "../images/Facebook-Icon.png";
+import instagramIcon from "../images/Instagram-Icon.png";
+import githubIcon from "../images/GitHub-Icon.png";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <img src={twitterIcon} alt="" />
+      <img src={facebookIcon} alt="" />
+      <img src={instagramIcon} alt="" />
+      <img src={githubIcon} alt="" />
+    </div>
+  );
 }
 
 export default Footer;
