cfd0ff0d98e114fba6bb360801efda65cb120d8e
Jared Mates <jaredmates45@gmail.com>
2022-04-07 15:13:53 -0700

    added content to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from "react";
+import ken from "../images/ken.jpg";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <img src={ken} alt="" />
+      <h1>Laura Smith</h1>
+      <h3>Frontend Developer</h3>
+      <a href="">laurasmith.website</a>
+      <button>Email</button>
+      <button>LinkedIn</button>
+    </div>
+  );
 }
 
 export default Header;
